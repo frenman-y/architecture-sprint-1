6c54118327eb3273332329c42b67eb6d2d9cd412
BlackDEK <BlackDEK546@yandex.ru>
2024-10-30 18:11:25 +0500

    - Добавлен flake.nix файл для работы с зависимостями.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flakeUtils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flakeUtils
+    }:
+    flakeUtils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      shellInput = with pkgs; [
+        # nix
+        nixpkgs-fmt
+        nil
+        statix
+        deadnix
+        clang-tools
+
+        # nodejs
+        nodejs
+        nodePackages.yarn
+        nodePackages.mocha
+      ];
+    in
+    {
+      devShells.default = pkgs.mkShell rec {
+        packages = shellInput;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
+      };
+    }
+    );
+}
