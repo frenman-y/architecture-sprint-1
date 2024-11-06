9030ce6650717f56fd5407a67338973973605217
BlackDEK <BlackDEK546@yandex.ru>
2024-11-06 18:51:42 +0500

    - Перенос кода в host микрофронт.

---
+++ frontend/microfrontend/host/webpack.config.js
@@ -0,0 +1,93 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin =
+    require('webpack/lib/container/ModuleFederationPlugin');
+const Dotenv = require('dotenv-webpack');
+
+const path = require('path');
+const deps = require('./package.json').dependencies;
+const printCompilationMessage = require('./compilation.config.js');
+
+module.exports = (_, argv) => ({
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    publicPath: 'http://localhost:3000/',
+    // publicPath: path.join(__dirname, 'dist'),
+  },  
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    watchFiles: [path.resolve(__dirname, 'src')],
+    onListening: function (devServer) {
+      const port = devServer.server.address().port
+
+      printCompilationMessage('compiling', port)
+
+      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+        setImmediate(() => {
+          if (stats.hasErrors()) {
+            printCompilationMessage('failure', port)
+          } else {
+            printCompilationMessage('success', port)
+          }
+        })
+      })
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'host',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new Dotenv()
+  ],
+});
