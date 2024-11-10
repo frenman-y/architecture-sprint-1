f71cbb1fd3bd9da2fe3986367782c03c1a07609f
DmitryGolitsyn <dgolitsyn@gmail.com>
2024-11-10 16:15:37 +0300

    sprint 1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,18 +3,18 @@ import { Route, useHistory, Switch } from "react-router-dom";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
-import ImagePopup from "./ImagePopup";
+import PopupWithForm from "../../microfrontend/photos-microfrontend/src/components/PopupWithForm";
+import ImagePopup from "../../microfrontend/photos-microfrontend/src/components/ImagePopup";
 import api from "../utils/api";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import EditProfilePopup from "./EditProfilePopup";
-import EditAvatarPopup from "./EditAvatarPopup";
-import AddPlacePopup from "./AddPlacePopup";
+import EditProfilePopup from "../../microfrontend/profile-microfrontend/src/components/EditProfilePopup";
+import EditAvatarPopup from "../../microfrontend/profile-microfrontend/src/components/EditAvatarPopup";
+import AddPlacePopup from "../../microfrontend/photos-microfrontend/src/components/AddPlacePopup";
 import Register from "./Register";
 import Login from "./Login";
 import InfoTooltip from "./InfoTooltip";
 import ProtectedRoute from "./ProtectedRoute";
-import * as auth from "../utils/auth.js";
+import * as auth from "../../microfrontend/auth-microfrontend/src/utils/auth.js";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
